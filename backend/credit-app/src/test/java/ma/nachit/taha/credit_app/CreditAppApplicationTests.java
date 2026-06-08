97e2fadd5c1085ccd7a6929a5e739f38dd80421a
Taha Nachit <tnnachit@gmail.com>
2026-06-08 09:17:07 +0100

    Initial project setup

---
+++ backend/credit-app/src/test/java/ma/nachit/taha/credit_app/CreditAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.nachit.taha.credit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
